5876dbeed8fffaab7e4179e163cad896c2a21e72
Thomas Eriksson Mueller <thomasem@kth.se>
2023-10-27 16:27:50 +0200

    working page

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-
-
+/**
+ * Project modals dynamically created from projects array
+ */
 const projects = [
-    {
-        title: "KTH Formula Student",
-        image: "images/kth_formula_student_logo.jpg",
-        description: "I've been a part of KTH Formula Student since 2022. I started out as a software developer in the Driverless division, and I am now the Head of Software Development in the Driverless division. Being Head of Software Development entails developing monitoring software for our autonomous vehicle, as well as developing software for our simulators. Further, I am also responsible for our in-house computing, networking, and storage infrastructure. Feel free to visit <a href=\"https://kthformulastudent.se/\">our website</a>"
-    },
-    {
-        title: "Flutter Game App",
-        image: "images/flutter.png",
-        description: "I am currently working with a friend on developing a game app using Flutter. The game is a simple 2D game where the player controls a character and tries to avoid obstacles. The game is developed using Flutter and Flame. The game is currently in development, and we am planning to release it on the Google Play Store and the Apple App Store in the near future. The inspiration to this project came from a Flame workshop we attended in fall of 2023"
-    },
-    {
-        title: "Museum Exhibition App",
-        image: "images/tekniska.png",
-        description: "As part of a course in Software Engineering, I together with 7 other students, developed an app for the Tekniska Museet in Stockholm for their Zero City exhibition. The app was developed using Flutter. This project also included leadership and teamwork, we had a rotating Scrum agile development team, so I served as Scrum Master and Project Owner during different sprints. During the remaining sprints, I was a developer. This project was conducted from the fall of 2021 to the spring of 2022"
-    },
-    {
-        title: "Internet Programming Game",
-        image: "images/roguelike.png",
-        description: "As part of a course in Programming Paradigms, I developed a Rougelike game with PyGame played through a server-client TCP socket communication. The game was developed in Python, using JSON serialization for the communication. The networking aspect allowed for multiple games and players be able to play in parallel through the server and network configurations. The game was developed during the spring of 2022."
-    }
+  {
+    title: "KTH Formula Student",
+    image: "images/kth_formula_student_logo.jpg",
+    description: "I've been a part of KTH Formula Student since 2022. I started out as a software developer in the Driverless division, and I am now the Head of Software Development in the Driverless division. Being Head of Software Development entails developing monitoring software for our autonomous vehicle, as well as developing software for our simulators. Further, I am also responsible for our in-house computing, networking, and storage infrastructure. Feel free to visit <a href=\"https://kthformulastudent.se/\">our website</a>"
+  },
+  {
+    title: "Flutter Game App",
+    image: "images/flutter.png",
+    description: "I am currently working with a friend on developing a game app using Flutter. The game is a simple 2D game where the player controls a character and tries to avoid obstacles. The game is developed using Flutter and Flame. The game is currently in development, and we am planning to release it on the Google Play Store and the Apple App Store in the near future. The inspiration to this project came from a Flame workshop we attended in fall of 2023"
+  },
+  {
+    title: "Museum Exhibition App",
+    image: "images/tekniska.png",
+    description: "As part of a course in Software Engineering, I together with 7 other students, developed an app for the Tekniska Museet in Stockholm for their Zero City exhibition. The app was developed using Flutter. This project also included leadership and teamwork, we had a rotating Scrum agile development team, so I served as Scrum Master and Project Owner during different sprints. During the remaining sprints, I was a developer. This project was conducted from the fall of 2021 to the spring of 2022"
+  },
+  {
+    title: "Internet Programming Game",
+    image: "images/roguelike.png",
+    description: "As part of a course in Programming Paradigms, I developed a Rougelike game with PyGame played through a server-client TCP socket communication. The game was developed in Python, using JSON serialization for the communication. The networking aspect allowed for multiple games and players be able to play in parallel through the server and network configurations. The game was developed during the spring of 2022."
+  }
 ];
 
 const projectContainer = document.getElementById("project-container");
 projects.forEach((project, index) => {
-    const projectCard = document.createElement("div");
-    projectCard.classList.add("col-md-4", "mb-4");
-    projectCard.innerHTML = `
+  const projectCard = document.createElement("div");
+  projectCard.classList.add("col-md-4", "mb-4");
+  projectCard.innerHTML = `
     <div class="card aos-animate" data-aos="fade-up" data-aos-delay="200">
         <div class="position-relative">
             <img src="${project.image}" class="card-img-top" alt="Project Image">
@@ -40,76 +41,131 @@ projects.forEach((project, index) => {
         </div>
     </div>
 `;
-    projectContainer.appendChild(projectCard);
+  projectContainer.appendChild(projectCard);
 
-    const modalButton = projectCard.querySelector("button[data-target='#project-modal']");
-    modalButton.addEventListener("click", () => {
-        const modalImage = document.getElementById("project-modal-image");
-        const modalDescription = document.getElementById("project-modal-description");
-        const modalTitle = document.getElementById("project-modal-title");
-        modalImage.src = project.image;
-        modalDescription.textContent = project.description;
-        modalTitle.textContent = project.title;
-    });
+  const modalButton = projectCard.querySelector("button[data-target='#project-modal']");
+  modalButton.addEventListener("click", () => {
+    const modalImage = document.getElementById("project-modal-image");
+    const modalDescription = document.getElementById("project-modal-description");
+    const modalTitle = document.getElementById("project-modal-title");
+    modalImage.src = project.image;
+    modalDescription.textContent = project.description;
+    modalTitle.textContent = project.title;
+  });
 });
 
+/**
+ * Typed JS on loading page in hero section
+ */
 document.addEventListener("DOMContentLoaded", function () {
-    var options = {
-        strings: ["Hey, I'm Thomas"],
-        typeSpeed: 50,
-        backSpeed: 25,
-        backDelay: 1000,
-        startDelay: 500,
-        showCursor: false,
-        cursorChar: '|',
-        loop: false
-    };
-    var options2 = {
-        strings: ["This is my personal website!"],
-        typeSpeed: 50,
-        backSpeed: 25,
-        backDelay: 1000,
-        startDelay: 2000,
-        showCursor: false,
-        cursorChar: '_',
-        loop: false
-    };
+  var options = {
+    strings: ["Hey, I'm Thomas"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    showCursor: false,
+    cursorChar: '|',
+    loop: false
+  };
+  var options2 = {
+    strings: ["This is my personal website!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 2000,
+    showCursor: false,
+    cursorChar: '_',
+    loop: false
+  };
 
-    var typed = new Typed("#typed-output-h1", options);
-    var typed = new Typed("#typed-output-p", options2);
+  var typed = new Typed("#typed-output-h1", options);
+  var typed = new Typed("#typed-output-p", options2);
 });
 
 function smoothScroll(event) {
-    // Prevent default anchor click behavior
-    event.preventDefault();
+  // Prevent default anchor click behavior
+  event.preventDefault();
 
-    // Get the target element
-    var target = document.querySelector(event.target.hash);
+  // Get the target element
+  var target = document.querySelector(event.target.hash);
 
-    // Calculate the distance to scroll
-    var distance = target.getBoundingClientRect().top;
+  // Calculate the distance to scroll
+  var distance = target.getBoundingClientRect().top;
 
-    // Scroll to the target element
-    window.scrollBy({
-        top: distance,
-        left: 0,
-        behavior: 'smooth'
-    });
+  // Scroll to the target element
+  window.scrollBy({
+    top: distance,
+    left: 0,
+    behavior: 'smooth'
+  });
 }
 
+/**
+ * Scroll to top button
+ */
 var scrollToTopButton = document.getElementById("scroll-to-top");
 
 window.onscroll = function () {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        scrollToTopButton.classList.remove("d-none");
-    } else {
-        scrollToTopButton.classList.add("d-none");
-    }
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    scrollToTopButton.classList.remove("d-none");
+  } else {
+    scrollToTopButton.classList.add("d-none");
+  }
 };
 
 scrollToTopButton.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+/**
+ * Mobile nav toggle
+ */
+document.querySelector('.mobile-nav-toggle').addEventListener('click', function (e) {
+  document.body.classList.toggle('mobile-nav-active');
+});
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let elementPos = document.querySelector(el).offsetTop;
+  window.scrollTo({
+    top: elementPos,
+    behavior: 'smooth'
+  });
+};
+
+/**
+ * Scrool with ofset on links with a class name .scrollto
+ */
+let scrollLinks = document.querySelectorAll('.scrollto');
+scrollLinks.forEach(scrollLink => {
+  scrollLink.addEventListener('click', function (e) {
+    if (document.querySelector(this.hash)) {
+      e.preventDefault();
+
+      let body = document.querySelector('body');
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active');
+        let navbarToggle = document.querySelector('.mobile-nav-toggle');
+        navbarToggle.classList.toggle('bi-list');
+        navbarToggle.classList.toggle('bi-x');
+      }
+      scrollto(this.hash);
+    }
+  }, true);
+});
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 800,
+  });
 });
